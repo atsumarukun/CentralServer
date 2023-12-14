1a604c9c34f47df5b8fe7149547e855ab87117f8
atsumarukun <atys.smz@gmail.com>
2023-12-14 16:49:15 +0900

    コンピュータ取得のクエリ変更.

--- api/internal/app/api/computer/infrastructure.go
+++ api/internal/app/api/computer/infrastructure.go
@@ -14,7 +14,7 @@ func NewComputerInfrastructure() repository.ComputerRepository {
 
 func (_ computerInfrastructure) GetComputerById(id int) (*domain.Computer, error) {
 	var computer domain.Computer
-	if err := pkg.DB.Find(&computer, id).Error; err != nil {
+	if err := pkg.DB.First(&computer, id).Error; err != nil {
 		return nil, err
 	}
 	return &computer, nil
