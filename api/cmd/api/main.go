862a7ac8725c5bc8979abfa42de91642dde7a240
atsumarukun <atys.smz@gmail.com>
2023-12-18 01:25:18 +0900

    corsの設定.

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"api/internal/app/api/pkg/database"
 	"api/internal/app/api/computer"
 )
@@ -10,6 +11,11 @@ func main() {
 	database.Connect()
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"*",
+		},
+	}))
 	v1 := r.Group("/api/v1")
 	computer.AddComputerRoutes(v1)
 	r.Run(":8000")
