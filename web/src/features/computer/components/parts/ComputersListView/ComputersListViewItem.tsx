60bd29ba9daa6b4fb91253d7df78b580fadacacc
atsumarukun <atys.smz@gmail.com>
2023-12-18 06:26:27 +0900

    コンピュータビューアイテムをクリック時ページ遷移.

--- web/src/features/computer/components/parts/ComputersListView/ComputersListViewItem.tsx
+++ web/src/features/computer/components/parts/ComputersListView/ComputersListViewItem.tsx
@@ -9,6 +9,7 @@ import {
   Icon,
   Text,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { MdCheckBoxOutlineBlank, MdMoreHoriz } from "react-icons/md";
 
 type Props = {
@@ -18,36 +19,37 @@ type Props = {
 export function ComputersListViewItem({ computer }: Props) {
   return (
     <HStack spacing={4} px={6} borderBottomWidth="1px">
-      <HStack
-        as={chakra.button}
-        textAlign="left"
-        spacing={4}
+      <Button size="xs" variant="unstyle">
+        <Icon as={MdCheckBoxOutlineBlank} boxSize={6} />
+      </Button>
+      <Grid
+        as={Link}
+        href={`/computer/${computer.id}`}
+        gap={4}
         py={3}
         flexGrow={1}
+        templateColumns="repeat(5, 1fr)"
       >
-        <Icon as={MdCheckBoxOutlineBlank} boxSize={6} />
-        <Grid flexGrow={1} templateColumns="repeat(5, 1fr)" gap={4}>
-          <GridItem colSpan={2}>
-            <Text>{computer.host_name}</Text>
-          </GridItem>
-          <GridItem>
-            <HStack>
-              <Circle
-                bgColor={computer.running ? "green.400" : "red.400"}
-                p={1}
-              />
-              <Text>{computer.running ? "Running" : "Stopped"}</Text>
-            </HStack>
-          </GridItem>
-          <GridItem>
-            <Text>{computer.ip_address}</Text>
-          </GridItem>
-          <GridItem>
-            <Text>{computer.mac_address}</Text>
-          </GridItem>
-        </Grid>
-      </HStack>
-      <Button size="xs" variant="unstyle">
+        <GridItem colSpan={2}>
+          <Text>{computer.host_name}</Text>
+        </GridItem>
+        <GridItem>
+          <HStack>
+            <Circle
+              bgColor={computer.running ? "green.400" : "red.400"}
+              p={1}
+            />
+            <Text>{computer.running ? "Running" : "Stopped"}</Text>
+          </HStack>
+        </GridItem>
+        <GridItem>
+          <Text>{computer.ip_address}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{computer.mac_address}</Text>
+        </GridItem>
+      </Grid>
+      <Button py={3} size="xs" variant="unstyle">
         <Icon as={MdMoreHoriz} boxSize={6} />
       </Button>
     </HStack>
