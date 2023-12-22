0e7e521ba5a91a95100731103fb6ec4d0ff6f179
atsumarukun <atys.smz@gmail.com>
2023-12-23 00:29:50 +0900

    ComputersListViewコンポーネントの内容をindex.tsxに移行.

--- web/src/features/computer/components/parts/ComputersListView/index.tsx
+++ web/src/features/computer/components/parts/ComputersListView/index.tsx
@@ -1,1 +1,19 @@
-export * from "./ComputersListView";
+import { Stack } from "@chakra-ui/react";
+import { ComputersListViewItem } from "./ComputersListViewItem";
+import { Computer } from "@/features/computer/computer";
+import { ComputersListViewHeader } from "./ComputersListViewHeader";
+
+type Props = {
+  computers: Computer[];
+};
+
+export function ComputersListView({ computers }: Props) {
+  return (
+    <Stack spacing={0} mx={4}>
+      <ComputersListViewHeader />
+      {computers.map((v) => (
+        <ComputersListViewItem computer={v} key={v.id} />
+      ))}
+    </Stack>
+  );
+}
