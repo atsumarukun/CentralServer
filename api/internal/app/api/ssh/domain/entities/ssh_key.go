f770c6e85a15e2a1f031df9dfb12e7f3663a35ed
atsumarukun <atys.smz@gmail.com>
2024-01-02 10:48:36 +0900

    SSH KEYのユニーク複合インデックスの名前を変更.

--- api/internal/app/api/ssh/domain/entities/ssh_key.go
+++ api/internal/app/api/ssh/domain/entities/ssh_key.go
@@ -8,10 +8,10 @@ import (
 type SshKey struct {
 	gorm.Model
 	ID           int            `gorm:"primary_key"`
-	UserName     string         `gorm:"not null;uniqueIndex:idx_member"`
+	UserName     string         `gorm:"not null;uniqueIndex:unique"`
 	PrivateKey   string         `gorm:"not null"`
   CreatedAt    time.Time
   UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
-	ComputerID   int            `gorm:"not null;uniqueIndex:idx_member"`
+	ComputerID   int            `gorm:"not null;uniqueIndex:unique"`
 }
