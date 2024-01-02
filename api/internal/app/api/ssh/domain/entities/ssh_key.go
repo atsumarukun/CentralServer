3decc35d26028aee5cdae01169b8d56cc092a3ab
atsumarukun <atys.smz@gmail.com>
2024-01-02 10:45:03 +0900

    SSH KEYのComputerIDとUserNameの組み合わせをユニークに変更.

--- api/internal/app/api/ssh/domain/entities/ssh_key.go
+++ api/internal/app/api/ssh/domain/entities/ssh_key.go
@@ -8,10 +8,10 @@ import (
 type SshKey struct {
 	gorm.Model
 	ID           int            `gorm:"primary_key"`
-	UserName     string         `gorm:"not null"`
+	UserName     string         `gorm:"not null;uniqueIndex:idx_member"`
 	PrivateKey   string         `gorm:"not null"`
   CreatedAt    time.Time
   UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
-	ComputerID   int            `gorm:"not null"`
+	ComputerID   int            `gorm:"not null;uniqueIndex:idx_member"`
 }
